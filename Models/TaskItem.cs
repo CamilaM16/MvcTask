1b1c823fdc163984a407c05b0376c33f6aeb8323
CamilaM16 <camila.malaga.60114@usalesiana.edu.bo>
2023-11-02 11:29:47 -0400

    Fixed Create Bug

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -8,7 +8,7 @@ public class TaskItem
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     [BsonElement("taskTitle")]
     public string? TaskTitle { get; set; }
